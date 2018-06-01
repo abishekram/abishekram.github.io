ba5f9a573f9953731034b596194e3e25f59d689e
Abishek ram <shekabi2@gmail.com>
2018-06-01 23:52:09 +0530

    Back ground color changed and Typed js added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+$(".pricing-table").hover(
+  function () {
+    $(this).addClass('pulse');
+  },
+  function () {
+    $(this).removeClass('pulse');
+  }
+  );
+
+//   var typed = new Typed('.rolling_skill', {
+//   strings: ["HTML , CSS ,JS","Java Frameworks "],
+//   typeSpeed: 30
+// });
+
+var namedType = new Typed( '.developer_anim span strong',{strings: [" a software developer" ," Abishek ram "],typeSpeed: 30});
